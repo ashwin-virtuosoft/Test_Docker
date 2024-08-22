6a2c06ab441ddb1273ca675f103eee3652571325
ashwin-virtuosoft <ashwin.kumar@virtuosoft.in>
2024-08-22 14:43:27 +0530

    Initial Commit

---
+++ TestDocker/Controllers/FileController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestDocker.Service;
+
+namespace TestDocker.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly FileService _fileService;
+        public FileController()
+        {
+            _fileService=new FileService();
+        }
+        [HttpGet]
+        [Route("GetFiles")]
+        public ActionResult<List<string>> GetNewerFiles(string folderPath, string fileName)
+        {
+            try
+            {
+                var newerFiles = _fileService.GetNewerFiles(folderPath, fileName);
+                return Ok(newerFiles);
+
+            }catch(FileNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ TestDocker/Service/FileService.cs
@@ -0,0 +1,22 @@
+﻿namespace TestDocker.Service
+{
+    public class FileService
+    {
+        public List<string> GetNewerFiles(string folderPath,string fileName)
+        {
+            string targetFilePath=Path.Combine(folderPath,fileName);
+            if(!File.Exists(targetFilePath))
+                throw new FileNotFoundException($"File {targetFilePath} does not exist");
+
+            DateTime targetTime=File.GetLastWriteTime(targetFilePath).ToUniversalTime();
+            var newerFiles=new List<string>();
+            foreach(string file in Directory.GetFiles(folderPath,fileName))
+            {
+                DateTime fileTime=File.GetLastWriteTime(file).ToUniversalTime();
+                if(file!=targetFilePath && fileTime > targetTime) { }
+                    newerFiles.Add(file);
+            }
+            return newerFiles;
+        }
+    }
+}
