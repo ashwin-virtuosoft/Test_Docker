72661bdff3f2ca05c23a084ceba6c08e114abf66
ashwin-virtuosoft <ashwin.kumar@virtuosoft.in>
2024-09-02 13:10:24 +0530

    FileWatcher
    Using FileWatcher to get the files and monitor

---
+++ TestDocker/Controllers/FileWatcherController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestDocker.Service;
+
+namespace TestDocker.Controllers
+{
+    public class FileWatcherController : Controller
+    {
+        private readonly FileWatcher _watcher;
+
+        public FileWatcherController(FileWatcher fileWatcher)
+        {
+            _watcher = fileWatcher;
+        }
+
+        [HttpGet("File-watcher")]
+        public ActionResult<List<string>> GetNewerFiles()
+        {
+            try
+            {
+                var files =  _watcher.GetModifiedFiles();
+                return Ok(files);
+            }
+            catch (FileNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }            
+        }
+    }
+}
---
+++ TestDocker/Service/FileWatcher.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.FileSystemGlobbing;
+using System.IO;
+
+namespace TestDocker.Service
+{
+    public class FileWatcher : IHostedService
+    {
+        private readonly List<string> modifiedFiles=new List<string>();
+        private FileSystemWatcher fileSystemWatcher;
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            fileSystemWatcher = new FileSystemWatcher(@"/app/TestFolder")
+            {
+                NotifyFilter = NotifyFilters.LastWrite |  NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                Filter = "*.txt",
+                EnableRaisingEvents = true
+            };
+
+            fileSystemWatcher.Changed += OnChanged;
+            fileSystemWatcher.Created += OnCreated;
+            fileSystemWatcher.Deleted += OnDeleted;
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            fileSystemWatcher.Dispose();
+            return Task.CompletedTask;
+        }
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+            modifiedFiles.Add(e.FullPath);
+        }
+
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File created: {e.FullPath}");
+            modifiedFiles.Add(e.FullPath);
+        }
+
+        private void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File deleted: {e.FullPath}");
+        }
+
+        public List<string> GetModifiedFiles()
+        {
+            return new List<string>(modifiedFiles);
+        }
+    }
+}
